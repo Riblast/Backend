bce8a5c2ddbf8781eb46e34227a906fed2a71ba1
Franco Fleitas <francofleitasguti@gmail.com>
2022-11-09 16:25:50 -0300

    Servidor con Express

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const Contenedor = require('./Clase')
+const express = require('express')
+
+const app = express()
+
+const PORT = 8080 || process.env.PORT
+
 const productos = new Contenedor('./productos.json')
 
-const ejecutar = async () =>{
+app.get('/', (req, res) => {
+    res.send(`<h1>Home</h1>`)
+})
 
-    const item1 = {
-        itemName: "Pizzeta integral",
-        itemPrice: 200,
-        url: "url"
-    }
+app.get('/productos', async (req, res) => {
 
-}
-ejecutar()
+    const prods = await productos.getAll()
+
+    res.send({productos: prods})
+})
+
+app.get('/productoRandom', async (req, res) => {
+    
+    const prods = await productos.getAll()
+    const randomProd = parseInt(Math.random() * prods.length)
+    
+    res.send({producto: prods[randomProd]})
+})
+
+const server = app.listen(PORT, () => {
+    console.log(`server iniciado en puerto ${PORT}`)
+})
+
+server.on('error', error => console.log('error en el servidor', error))
